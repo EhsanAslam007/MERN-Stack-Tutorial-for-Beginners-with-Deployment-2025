93a0ca0e0ebf2e4423dff39e67d2eb38b79aabc2
EhsanAslam007 <khawajaehsan700@gmail.com>
2026-03-26 23:20:34 +0500

    ehsan changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import CreatePage from "./pages/CreatePage"
 import NoteDetailPage from "./pages/NoteDetailPage"
@@ -8,7 +8,8 @@ import toast from 'react-hot-toast'
 const App = () => {
   return (
 
-    <div data-theme="forest">
+    <div className='relative min-h-screen w-full'>
+      <div className="absolute inset-0 -z-10 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
